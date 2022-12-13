c77d20caa07024072a17f2ac62a5ace6e750cc60
Akeem <akeem.mighty@gmail.com>
2022-12-13 17:54:44 -0500

    Create Horizontal Line Of Z Pattern

--- zPattern.java
+++ zPattern.java
@@ -1,5 +1,18 @@
 public class zPattern {
     public static void main(String[] args) {
-        
+        int numTiles = 8;
+        System.out.println("<--Start Program-->");
+        for(int i=0; i<numTiles; i++)
+        {
+            for(int j=0; j<numTiles; j++)
+            {
+                //Create the Horizontal Lines
+                if(i==0 || i==numTiles-1)
+                {
+                    System.out.print("1");
+                }
+            }
+        }
+        System.out.println("<--End Program-->");
     }
 }
